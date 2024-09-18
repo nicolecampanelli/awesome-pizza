bfecf9ea7bd989e36486d787dda24b1eebe52d45
nicole_campanelli <nicole.campanelli@vidyasoft.it>
2024-09-18 11:27:44 +0200

    fixed bottom covered from footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,22 @@ import PizzaCard from '@/components/cards/PizzaCard'
 import FooterComponent from '@/components/footer/FooterComponent'
 import { getAllPizzas } from '@/services/pizzas/PizzaService'
 
-
 const PizzaListPage = (): ReactElement => {
   const [pizzas, setPizzas] = useState<any[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<string>('')
 
   const performGetAllPizzas = async () => {
-    await getAllPizzas().then((res) => {
-      setPizzas(res.data)
-      console.log(res.data)
-    }).catch((err) => {
-      setError('Failed to fetch pizzas')
-    }).finally(() => {
-      setLoading(false)
-    })
-
+    await getAllPizzas()
+      .then((res) => {
+        setPizzas(res.data)
+      })
+      .catch(() => {
+        setError('Failed to fetch pizzas')
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   useEffect(() => {
@@ -30,18 +30,21 @@ const PizzaListPage = (): ReactElement => {
   if (error) return <p>{error}</p>
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
-      {pizzas.map((pizza) => (
-        <PizzaCard
-          key={pizza.id}
-          pizzaName={pizza.pizzaName}
-          pizzaDescription={pizza.pizzaDescription}
-          price={pizza.price}
-        />
-      ))}
+    <div className='flex flex-col min-h-screen pb-20'>
+      <div className='flex-grow'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
+          {pizzas.map((pizza) => (
+            <PizzaCard
+              key={pizza.id}
+              pizzaName={pizza.pizzaName}
+              pizzaDescription={pizza.pizzaDescription}
+              price={pizza.price}
+            />
+          ))}
+        </div>
+      </div>
       <FooterComponent />
     </div>
-
   )
 }
 
